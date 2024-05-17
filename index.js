1d16ff729449e34c2ff2e01201313146d08ef8e6
SreeTeja <gundasreeteja@gmail.com>
2024-05-16 22:50:20 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,39 @@
+for (i = 0; i < document.querySelectorAll(".drum").length; i++) {
+  document.querySelectorAll("button")[i].addEventListener("click", function () {
+    util(this.innerHTML);
+  });
+}
+
+document.addEventListener("keypress", function (event) {
+  util(event.key);
+});
+
+function util(letter) {
+  switch (letter) {
+    case "w":
+      audioPlay("./sounds/drum1.mp3");
+      break;
+    case "a":
+      audioPlay("./sounds/drum2.mp3");
+      break;
+    case "s":
+      audioPlay("./sounds/drum3.mp3");
+      break;
+    case "d":
+      audioPlay("./sounds/drum4.mp3");
+      break;
+    case "j":
+      audioPlay("./sounds/snare.mp3");
+      break;
+    case "k":
+      audioPlay("./sounds/kick.mp3");
+      break;
+    case "l":
+      audioPlay("./sounds/crash.mp3");
+      break;
+  }
+}
+
+function audioPlay(fileLocation) {
+  new Audio(fileLocation).play();
+}
